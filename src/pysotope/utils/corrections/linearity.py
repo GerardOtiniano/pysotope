3669f56d517cb48a6bdf16cd6c6bc723d1ee37c4
@GerardOtiniano <gerardotiniano@gmail.com>
2025-04-22 12:39:39 -0400

    Fix create_subfolder error

--- src/pysotope/utils/corrections/linearity.py
+++ src/pysotope/utils/corrections/linearity.py
@@ -3,6 +3,7 @@ import pandas as pd
 from . .queries import *
 from . .queries import neg_response
 from . .figures import *
+from . .base_functions import create_subfolder
 from IPython.display import clear_output
 from statsmodels.sandbox.regression.predstd import wls_prediction_std
 from . .curve_fitting import *
@@ -55,8 +56,8 @@ def process_linearity_correction(cfg, samp, drift, lin_std, user_choice, correct
 
     user_input = input("\nDoes this look correct? (Y/N)\n").lower()
     if pos_response(user_input):
-        cfg.linearity_applied = True 
-        lin_std, drift, samp, excluded_drift, excluded_lin_std, excluded_samp  = linearity_correction(drift, samp, lin_std, norm, chain_corr_val, 
+        cfg.linearity_applied = True
+        lin_std, drift, samp, excluded_drift, excluded_lin_std, excluded_samp  = linearity_correction(drift, samp, lin_std, norm, chain_corr_val,
                                                                                                       dD_id, folder_path, fig_path, log_file_path=log_file_path)
         append_to_log(log_file_path, "- Minimum peak area to derive linearity correction: "+str(chain_corr_val))
         append_to_log(log_file_path, "- Number of drift standards excluded because of below threshold area: "+str(len(excluded_drift)))
@@ -117,7 +118,7 @@ def linearity_correction(drift, samp, lin_std, lin_norm, area_cutoff, dD_id, fol
     else:  # "growth"
         y_fit = exp_growth(xdata, *popt)
         parameter_text = f"y = {popt[0]} (1-e^(-{popt[1]})x + {popt[2]})"
-        
+
 
     # Total Sum of Squares and R²
     tss = np.sum((ydata - ydata.mean()) ** 2)
@@ -129,31 +130,31 @@ def linearity_correction(drift, samp, lin_std, lin_norm, area_cutoff, dD_id, fol
     append_to_log(log_file_path, f"- Best fit model type: {best_model}")
     append_to_log(log_file_path, f"- {best_model} model: {parameter_text}")
     append_to_log(log_file_path, f"- {best_model} model stats: R²: {r_squared:.3f} SSE: | {sse:.3f}")
-    
+
 
     # Determine reference value from the top 20% (by area) of the linearity standards
     lin_top_sort = filtered_lin_norm.sort_values(by='area', ascending=False)
     top_count = max(int(len(lin_top_sort) * 0.2), 1)
     lin_top_qt_area = lin_top_sort.head(top_count)
     lin_reference = lin_top_qt_area[dD_id].mean()  # average δD of top 20%
-    
+
     # Correct linearity standard
-    offset = y_fit - lin_reference # (predicted lin_std dD from peak area) - (dD of top 20% peak areas); difference between predicted and 'true values', giving offset 
+    offset = y_fit - lin_reference # (predicted lin_std dD from peak area) - (dD of top 20% peak areas); difference between predicted and 'true values', giving offset
     filtered_lin_std['linearity_corrected_dD']   = filtered_lin_std[dD_id] + offset  # Add offset to dD values from previous step
     filtered_lin_std['linearity_error'] = pred_error
-    
+
     if best_model == "linear":
         drift_est = linear_func(np.array(filtered_drift.area), *popt)
     elif best_model == "decay":
         drift_est = exp_decay(np.array(filtered_drift.area), *popt)
     elif best_model == "growth":
         drift_est = exp_growth(np.array(filtered_drift.area), *popt)
-        
+
     pred_error_drift = prediction_std(best_model, np.array(filtered_drift.area), popt, pcov, nsigma=2)
     drift_cor = drift_est - lin_reference + filtered_drift[dD_id] # (Predicted drift) -(reference value = offset. Then (offset) + (drift value) = corrected _drift
     filtered_drift['linearity_corrected_dD'] = drift_cor[~np.isnan(drift_cor)]
     filtered_drift['linearity_error'] = pred_error_drift
-    
+
     # Apply correction to samples
     filtered_samp = samp[samp['area'] >= area_cutoff].copy()
     if best_model == "linear":
@@ -162,7 +163,7 @@ def linearity_correction(drift, samp, lin_std, lin_norm, area_cutoff, dD_id, fol
         samp_est = exp_decay(np.array(filtered_samp.area), *popt)
     elif best_model == "growth":
         samp_est = exp_growth(np.array(filtered_samp.area), *popt)
-        
+
     pred_error_samp = prediction_std(best_model, np.array(filtered_samp.area), popt, pcov, nsigma=2)
     samp_cor = samp_est - lin_reference + filtered_samp[dD_id]
     filtered_samp['linearity_corrected_dD'] = samp_cor[~samp_cor.isna()]
